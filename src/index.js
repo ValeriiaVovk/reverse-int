e1df94d2c29b307724a272070ab87795b7f934dc
ValeriiaVovk <vovkvaleriya15@gmail.com>
2024-08-02 16:45:30 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let dataStr = n.toString();
+    let num = '';
+
+    let startIndex = dataStr[0] === '-' ? 1 : 0;
+
+    for (let i = dataStr.length-1; i >= startIndex; i--) {
+        num = num + dataStr[i];
+    }
+
+    return Number(num);
 }
